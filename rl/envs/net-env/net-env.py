4a7450f367fa1c5438e3f1d40cd6620e239b8af4
danisrcnl <danielesarcinella@outlook.com>
2022-03-07 11:42:40 +0000

    skeleton

--- rl/envs/net-env/net-env.py
+++ rl/envs/net-env/net-env.py
@@ -1,25 +1,35 @@
 import gym
 from gym import spaces
 
+class State:
+    def __init__(self):
+
+
 class NetEnv (gym.Env):
 
     NPORTS = 2
     STATE_FEATURES = 3
 
-    state = [0, 0, 0]
-
     def __init__ (self):
         self.action_space = spaces.Discrete(NPORTS)
         self.observation_space = spaces.Box(STATE_FEATURES)
+        self.state = State()
 
     def step (self, action):
         # action contains the # of the port the packet must be forwarded to
-        
-        done = True
+
+        # send action back on socket
+
+        # listen on socket
+            # as msg arrives store fields in state(t + 1) and reward(t)
+
+        done = False
         info = {}
         return self.state, reward, done, info
 
     def reset (self):
+        # listen on socket
+            # as msg arrives store fields in state, drop reward
 
     def render ():
 
