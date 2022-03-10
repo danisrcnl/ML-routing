ee8bb93b74db5d875ddca84ffec1d003fe167ce1
danisrcnl <danielesarcinella@outlook.com>
2022-03-10 11:03:32 +0000

    action history and class State

--- rl/envs/net-env/net-env.py
+++ rl/envs/net-env/net-env.py
@@ -1,8 +1,64 @@
 import gym
 from gym import spaces
+import socket
+import struct
+import itertools
+
+PORT, HOST_IP = 1500, '0.0.0.0'
+
+actionHistory = {}
+def addAction(curDst, action):
+    if actionHistory.get(curDst) is None:
+        actionHistory[curDst] = [action]
+    else:
+        actionHistory[curDst].append(action)
+def getActions(curDst):
+    if actionHistory.get(curDst) is None:
+        return []
+    else:
+        return actionHistory[curDst]
 
 class State:
     def __init__(self):
+        self.destinations = FutureDestinations()
+        self.prevActions = []
+    def setDsts(self, destinations):
+        self.destinations = destinations
+        self.prevActions = getActions(self.destinations.getCurDst())
+    def show(self):
+        print("Future destinations: ")
+        self.destinations.printL()
+        print("Previous actions: ", self.prevActions)
+
+class FutureDestinations:
+    def __init__(self):
+        self.curDst = 0
+        self.size = 0
+        self.dsts = []
+
+    def pushDst(self, dst):
+        self.dsts.append(int(dst))
+
+    def setSize(self, size):
+        self.size = int(size)
+
+    def setCurDst(self, dst):
+        self.curDst = int(dst)
+
+    def getCurDst(self):
+        return self.curDst
+
+    def reset(self):
+        self.dsts = []
+        self.curDst = 0
+        self.size = 0
+
+    def show(self):
+        print("Number of future destinations:", self.size)
+        print("Current destination:", self.curDst)
+        print("List of future destinations:")
+        for dst in self.dsts:
+            print(dst)
 
 
 class NetEnv (gym.Env):
