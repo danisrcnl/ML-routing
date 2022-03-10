14ba469d34c1f8e10fcf5609ec80560f769b8bc1
danisrcnl <danielesarcinella@outlook.com>
2022-03-10 11:15:40 +0000

    class Packet and def parse_req

--- rl/envs/net-env/net-env.py
+++ rl/envs/net-env/net-env.py
@@ -7,59 +7,88 @@ import itertools
 PORT, HOST_IP = 1500, '0.0.0.0'
 
 actionHistory = {}
-def addAction(curDst, action):
+def addAction (curDst, action):
     if actionHistory.get(curDst) is None:
         actionHistory[curDst] = [action]
     else:
         actionHistory[curDst].append(action)
-def getActions(curDst):
+def getActions (curDst):
     if actionHistory.get(curDst) is None:
         return []
     else:
         return actionHistory[curDst]
 
-class State:
-    def __init__(self):
-        self.destinations = FutureDestinations()
-        self.prevActions = []
-    def setDsts(self, destinations):
-        self.destinations = destinations
-        self.prevActions = getActions(self.destinations.getCurDst())
-    def show(self):
-        print("Future destinations: ")
-        self.destinations.printL()
-        print("Previous actions: ", self.prevActions)
-
 class FutureDestinations:
-    def __init__(self):
+    def __init__ (self):
         self.curDst = 0
         self.size = 0
         self.dsts = []
 
-    def pushDst(self, dst):
+    def pushDst (self, dst):
         self.dsts.append(int(dst))
 
-    def setSize(self, size):
+    def setSize (self, size):
         self.size = int(size)
 
-    def setCurDst(self, dst):
+    def setCurDst (self, dst):
         self.curDst = int(dst)
 
-    def getCurDst(self):
+    def getCurDst (self):
         return self.curDst
 
-    def reset(self):
+    def reset (self):
         self.dsts = []
         self.curDst = 0
         self.size = 0
 
-    def show(self):
+    def show (self):
         print("Number of future destinations:", self.size)
         print("Current destination:", self.curDst)
         print("List of future destinations:")
         for dst in self.dsts:
             print(dst)
 
+class State:
+    def __init__ (self):
+        self.destinations = FutureDestinations()
+        self.prevActions = []
+    def setDsts (self, destinations):
+        self.destinations = destinations
+        self.prevActions = getActions(self.destinations.getCurDst())
+    def show (self):
+        print("Future destinations: ")
+        self.destinations.printL()
+        print("Previous actions: ", self.prevActions)
+
+class Packet:
+    def __init__ (self, destinations, reward):
+        self.futureDestinations = destinations
+        self.reward = reward
+
+    def getDsts (self):
+        return self.futureDestinations
+
+    def getReward (self):
+        return self.reward
+
+
+def parse_req (data):
+    strdata = data.decode('UTF-8')
+    print(strdata)
+    parsed = strdata.split(' ')
+    if parsed[0] == 'GETP':
+        destinations = FutureDestinations()
+        size = int(parsed[1])
+        destinations.setSize(size)
+        dst = int(parsed[2])
+        destinations.setCurDst(dst)
+        lastRw = int(parsed[3])
+        max = 4 + size
+        for i in range(4, max):
+            destinations.pushDst(parsed[i])
+        print("Last reward:", lastRw)
+        pkt = Packet(destinations, lastRw)
+        return pkt
 
 class NetEnv (gym.Env):
 
@@ -75,6 +104,7 @@ class NetEnv (gym.Env):
         # action contains the # of the port the packet must be forwarded to
 
         # send action back on socket
+        # store action in action history
 
         # listen on socket
             # as msg arrives store fields in state(t + 1) and reward(t)
