93532c495afb666f361b038948443ca110403e8a
danisrcnl <danielesarcinella@outlook.com>
2022-03-23 17:11:07 +0000

    bounded queues

--- rl/net-env/net_env/envs/net_env.py
+++ rl/net-env/net_env/envs/net_env.py
@@ -3,13 +3,24 @@ from gym import spaces
 import socket
 import struct
 import itertools
+import collections
+import numpy as np
 
 PORT, HOST_IP = 1500, '0.0.0.0'
+MAX = 4294967295
+
+def fill (vec, max):
+    if (len(vec) < max):
+        for i in range (0, max - len(vec)):
+            vec.append(0)
+        return vec
+    else:
+        return vec
 
 actionHistory = {}
 def addAction (curDst, action):
     if actionHistory.get(curDst) is None:
-        actionHistory[curDst] = [action]
+        actionHistory[curDst] = collections.deque([action], 5)
     else:
         actionHistory[curDst].append(action)
 def getActions (curDst):
@@ -18,11 +29,12 @@ def getActions (curDst):
     else:
         return actionHistory[curDst]
 
+
 class FutureDestinations:
     def __init__ (self):
         self.curDst = 0
-        self.size = 0
-        self.dsts = []
+        self.size = 5
+        self.dsts = collections.deque(maxlen = 5)
 
     def pushDst (self, dst):
         self.dsts.append(int(dst))
@@ -62,15 +74,12 @@ class State:
         return self.destinations.getCurDst()
     def show (self):
         print("Future destinations: ")
-        self.destinations.printL()
+        self.destinations.show()
         print("Previous actions: ", self.prevActions)
     def makeNPArray (self):
-        array = np.array()
-        array.append(self.destinations.getCurDst())
-        for dst in self.destinations.getDsts():
-            array.append(dst)
-        for action in self.prevActions:
-            array.append(action)
+        firstrow = []
+        firstrow.append(self.destinations.getCurDst())
+        array = np.array(object = [fill(firstrow, 5), fill(self.destinations.getDsts(), 5), fill(self.prevActions, 5)])
         return array
 
 
@@ -93,7 +102,7 @@ def parse_req (data):
     if parsed[0] == 'GETP':
         destinations = FutureDestinations()
         size = int(parsed[1])
-        destinations.setSize(size)
+        #destinations.setSize(size) meant to be always 50
         dst = int(parsed[2])
         destinations.setCurDst(dst)
         lastRw = int(parsed[3])
@@ -108,20 +117,21 @@ class NetEnv (gym.Env):
 
     def __init__ (self):
         self.action_space = spaces.Discrete(2) #tb changed
-        self.observation_space = spaces.Box(3,) #tb changed
+        self.observation_space = spaces.Box(low = 0, high = MAX, shape=(3, 5)) #tb changed
         self.state = State()
-        self.pkt = Packet()
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            self.s = s
-            self.s.bind((HOST_IP, PORT))
-            self.s.listen()
-            while True:
-                print("listening...")
-                self.conn, self.addr = self.s.accept()
-                print("connected by", addr)
-                return
+        self.pkt = Packet(0, 0)
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.s.bind((HOST_IP, PORT))
+        self.s.listen()
+        while True:
+            print("listening...")
+            self.conn, self.addr = self.s.accept()
+            print("connected by", self.addr)
+            return
 
     def step (self, action):
+
+        info = {}
         # action contains the # of the port the packet must be forwarded to
 
         # send action back on socket
@@ -129,6 +139,7 @@ class NetEnv (gym.Env):
         ret = action
         addAction(self.state.getCurDst(), action)
         sendBack = struct.pack('I', ret)
+        print(self.conn)
         self.conn.sendall(sendBack)
 
         # listen on socket
@@ -142,12 +153,13 @@ class NetEnv (gym.Env):
             self.pkt = parse_req(data)
             self.state.setDsts(pkt.getDsts())
         except Exception:
-            self.conn.close()
-            self.s.close()
+            print("EXCEPTION!!!")
+            return self.state, self.pkt.getReward(), True, info
+            #self.conn.close()
+            #self.s.close()
 
         done = False
-        info = {}
-        return self.state, self.pkt.getReward(), done, info
+        return self.state.makeNPArray(), self.pkt.getReward(), done, info
 
     def reset (self):
         # listen on socket
